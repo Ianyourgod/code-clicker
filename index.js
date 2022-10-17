c410ad42d26b081f7a561f78bf2c66b6a06336cf
Ianyourgod <104151459+Ianyourgod@users.noreply.github.com>
2022-10-17 10:45:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-
+let money = 0;
+let lines = 0;
+let narration = 1;
+let writing = false
+let moneyValue = document.getElementById("money");
+let linesValue = document.getElementById("lines");
+let narrator = document.getElementById("narrator");
+const narratelines = ["You found an easter egg! :)", "Hello, and welcome to code clicker! You have decided you want to become a coder. Click the 'Write code' button to write your first line!"];
+// check updates
+setInterval(() => {
+    if (lines > 4 && narration>=1){
+        moneyValue.value = "Money: " + money.toString();
+    }
+    linesValue.value = "Lines: " + lines.toString()
+    narrator.value = narratelines[narration];
+    
+},1)
+//write a line
+function startwriting() {
+    writing = true
+    
+    //set the button to "writing... 8.0 seconds remaining"
+}
